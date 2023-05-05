ccf512913ba045677770f8013d150f86af39b1d0
Bhushan Mokal <bhushanmokal02@gmail.com>
2023-05-05 16:48:07 +0530

    Initial Commit

---
+++ StackDemo/StackDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackDemoApp.swift
+//  StackDemo
+//
+//  Created by Bhushan Mokal on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct StackDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
