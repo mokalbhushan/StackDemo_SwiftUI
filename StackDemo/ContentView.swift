c26d784bb03cb2f8bf527a874858a344ba50b162
Bhushan Mokal <bhushanmokal02@gmail.com>
2023-05-08 12:46:01 +0530

    vhz stack UI added

--- StackDemo/ContentView.swift
+++ StackDemo/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack{
+           Spacer()
+            //VStack
+            VStack {
+                Image("MILogo")
+                    .resizable()
+                    .frame(width: 200,height: 100,alignment:.center)
+                    .foregroundColor(.accentColor)
+                Text("Mumbai Indians")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.yellow)
+            }
+            .padding()
+            .background(Color.blue)
+            Text("VStack (Vertical Stack)")
+                .bold()
+            
+            Spacer()
+            
+            //HStack
+            HStack{
+                Image("MILogo")
+                    .resizable()
+                    .frame(width: 100,height: 50,alignment: .center)
+                Text("Mumbai Indians")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.yellow)
+            }
+            .padding()
+            .background(Color.blue)
+            Text("HStack (Horizontal Stack)")
+                .bold()
+            
+            Spacer()
+            
+            //ZStack
+            ZStack{
+                Image("MILogo")
+                    .resizable()
+                    .frame(width: 200,height: 100,alignment: .center)
+                Text("Mumbai Indians")
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.white)
+               
+            }
+            .padding()
+            .background(Color.blue)
+            Text("ZStack")
+                .bold()
+            Spacer()
         }
-        .padding()
     }
 }
 
